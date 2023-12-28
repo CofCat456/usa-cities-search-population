6726fa1665c2bb41b371fd60f426bd1b8019dcac
CofCat456 <a25690756@gmail.com>
2023-12-28 15:19:58 +0800

    chore: add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        'dark-text': '#E5E7EB',
+        'dark-bg': '#121212',
+        'comment': '#758575DD',
+        'highlight' : {
+          '1': '#B07D48',
+          '2': '#80A665',
+          '3': '#CB7676'
+        }
+      }
+    },
+  },
+  plugins: [],
+};
